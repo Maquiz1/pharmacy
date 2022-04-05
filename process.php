ccc5d4aa01c50a2d3f3b860a2787bd1a23dafc28
frdrckaman <frdrckaman@gmail.com>
2022-04-05 09:09:43 +0300

    fix drug assignment

--- process.php
+++ process.php
@@ -26,11 +26,10 @@ if($_GET['cnt'] == 'region'){
     <option value="">Select Batch</option>
     <?php foreach ($batches as $batch){?>
         <option value="<?=$batch['id']?>"><?=$batch['name']?></option>
-<?php }}elseif ($_GET['cnt'] == 'a_batch'){?>
-    <?php
+<?php }}elseif ($_GET['cnt'] == 'a_batch'){
     $a_batch=$override->get('batch', 'id', $_GET['getUid'])[0];
-    $a_study_staff=$override->get('staff_study', 'study_id', $a_batch['study_id'])[0];
-    $a_desc=$override->getNews('batch_description', 'batch_id',$_GET['getUid'], 'status', 1);?>
+    $a_study_staff=$override->get('staff_study', 'study_id', $a_batch['study_id']);
+    $a_desc=$override->getNews('batch_description', 'batch_id',$_GET['getUid'], 'status', 1)?>
     <div class="row-form clearfix">
         <div class="col-md-3">Drug</div>
         <div class="col-md-9">
@@ -46,11 +45,12 @@ if($_GET['cnt'] == 'region'){
         <div class="col-md-3">Staff</div>
         <div class="col-md-9">
             <select name="staff" style="width: 100%;" required>
-                <option value="">Select</option>
+                <option value="">Select </option>
                 <?php foreach ($a_study_staff as $staff){$stf=$override->get('user','id',$staff['staff_id'])[0]?>
                     <option value="<?=$stf['id']?>"><?=$stf['firstname'].' '.$stf['lastname']?></option>
                 <?php }?>
             </select>
         </div>
     </div>
+
 <?php }?>
